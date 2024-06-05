bcb480ccd74edcaf54a495b6040a8d8c4ab62d03
Ezequiel <ezequielgiribarren@gmail.com>
2024-06-05 17:54:07 -0300

    CONFIG

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
-const FRONTEND_URL = "http://localhost:5173";
-const PORT = 5000; // Define el puerto aquí
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const PORT = process.env.PORT || 5000; // Define el puerto aquí
 
 module.exports = {
     FRONTEND_URL,
